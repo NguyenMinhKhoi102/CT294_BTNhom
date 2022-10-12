8b6e2af00a2cc1bf52af5f931af06c255dd755a2
B1910241 <khoib1912041@student.ctu.edu.vn>
2022-10-12 17:08:04 +0700

    modified index.py

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 #import
+from cProfile import label
 import pandas as pd;
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
@@ -23,6 +25,20 @@ DT_gini.fit(X_train, y_train);
 y_pred = DT_gini.predict(X_test);
 
 print ("Accuracy is ", accuracy_score(y_test, y_pred) * 100);
-print ("array(",confusion_matrix(y_test, y_pred),")");
+mm = np.unique(y_test);
+print ("array(",confusion_matrix(y_test, y_pred, labels = mm),")");
+
+#Nghi thuc k-fold
+kf = KFold(n_splits=100, shuffle = True, random_state = 6000);
+acc_DT = 0;
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X.iloc[train_index], X.iloc[test_index];
+    y_train, y_test = y.iloc[train_index], y.iloc[test_index];
+    DT_gini.fit(X_train, y_train);
+    y_pred = DT_gini.predict(X_test);
+    acc_DT += accuracy_score(y_test, y_pred);
+print("Độ chính xác trung bình: ", acc_DT/100*100);
+
+
 
 
